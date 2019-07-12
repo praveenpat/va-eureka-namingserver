3756e9f6736ec06d69d2ce26186ff68a809175d4
praveen pattanshetti <praveen@praveens-MBP.home>
2019-07-12 08:17:12 -0400

    update

---
+++ src/test/java/com/discovery/eureka/vaeurekanamingserver/VaEurekaNamingserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.discovery.eureka.vaeurekanamingserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaEurekaNamingserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
